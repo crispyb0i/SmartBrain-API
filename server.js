8efe066a7cb15341ff11addaa993b4d1f9c0aabc
David Shin <davidshin@Davids-MacBook-Pro.local>
2020-07-26 16:16:34 -0700

    bug fix

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-const Clarifai = require('clarifai');
 
 const db = knex({
   client: 'pg',
